32fcdb6da1b9b3a8bc4d328be5231a0051044823
thomasridd <twridd@gmail.com>
2023-10-31 09:41:26 +0000

    update test comments

--- server/testutils/factories/baseClient.ts
+++ server/testutils/factories/baseClient.ts
@@ -8,7 +8,7 @@ export default Factory.define<BaseClient>(() => ({
   accessTokenValidity: 3600,
   scopes: ['read', 'write'],
   grantType: 'client_credentials',
-  audit: faker.lorem.lines(4),
+  audit: 'audit notes',
   count: 1,
   clientCredentials: {
     authorities: ['ROLE_CLIENT_CREDENTIALS'],
