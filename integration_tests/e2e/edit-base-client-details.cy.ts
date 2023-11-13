dbbdb7f02ea81cf31993615d1bcd25d534b36df1
thomasridd <twridd@gmail.com>
2023-11-13 11:38:00 +0000

    edit base client details testing

---
+++ integration_tests/e2e/edit-base-client-details.cy.ts
@@ -0,0 +1,86 @@
+import Page from '../pages/page'
+import EditBaseClientDetailsPage from '../pages/editBaseClientDetails'
+import ViewBaseClientPage from '../pages/viewBaseClient'
+
+const visitEditBaseClientDetailsPage = (): EditBaseClientDetailsPage => {
+  cy.signIn({ failOnStatusCode: true, redirectPath: '/base-clients/base_client_id_1/edit' })
+  return Page.verifyOnPage(EditBaseClientDetailsPage)
+}
+
+context('Edit base client details page', () => {
+  let editBaseClientDetailsPage: EditBaseClientDetailsPage
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubManageUser')
+    cy.task('stubListBaseClients')
+    cy.task('stubGetBaseClient')
+    cy.task('stubGetListClientInstancesList')
+    editBaseClientDetailsPage = visitEditBaseClientDetailsPage()
+  })
+
+  it('User can see base-client form inputs', () => {
+    editBaseClientDetailsPage.baseClientIdInput().should('exist')
+    editBaseClientDetailsPage.baseClientServiceRadioButton().should('exist')
+    editBaseClientDetailsPage.baseClientPersonalRadioButton().should('exist')
+    editBaseClientDetailsPage.baseClientAccessTokenValidityDropdown().should('be.visible')
+    editBaseClientDetailsPage.baseClientApprovedScopesInput().should('be.visible')
+  })
+
+  it('User can see audit trail form inputs', () => {
+    editBaseClientDetailsPage.auditTrailDetailsInput().should('be.visible')
+  })
+
+  it('User can see grant details form inputs', () => {
+    editBaseClientDetailsPage.grantTypeInput().should('be.visible')
+    editBaseClientDetailsPage.grantAuthoritiesInput().should('be.visible')
+    editBaseClientDetailsPage.grantDatabaseUsernameInput().should('be.visible')
+  })
+
+  it('User can see config form inputs', () => {
+    editBaseClientDetailsPage.configDoesExpireCheckbox().should('exist')
+    editBaseClientDetailsPage.configAllowedIpsInput().should('be.visible')
+  })
+
+  context('Access token validity dropdown', () => {
+    it('Custom input is initially hidden', () => {
+      editBaseClientDetailsPage.baseClientAccessTokenValidityInput().should('not.be.visible')
+    })
+
+    it('Custom input is shown when custom option is selected', () => {
+      editBaseClientDetailsPage.baseClientAccessTokenValidityDropdown().select('Custom')
+      editBaseClientDetailsPage.baseClientAccessTokenValidityInput().should('be.visible')
+    })
+  })
+
+  context('Allow client to expire ', () => {
+    it('Does expire checkbox is unchecked by default', () => {
+      editBaseClientDetailsPage.configDoesExpireCheckbox().should('not.be.checked')
+    })
+
+    it('Expiry days input is shown if checkbox is selected', () => {
+      editBaseClientDetailsPage.configDoesExpireCheckbox().click()
+      editBaseClientDetailsPage.configExpiryDaysInput().should('be.visible')
+    })
+  })
+
+  it('User clicks cancel to return to base-client screen', () => {
+    editBaseClientDetailsPage.cancelLink().click()
+    Page.verifyOnPage(ViewBaseClientPage)
+  })
+
+  it('User clicks continue to post new client details screen', () => {
+    // enter new audit details
+    editBaseClientDetailsPage.auditTrailDetailsInput().clear()
+    editBaseClientDetailsPage.auditTrailDetailsInput().type('updated')
+
+    // set up to check the POST request
+    cy.intercept('POST', '/base-clients/base_client_id_1/edit', req => {
+      const { body } = req
+      expect(body).to.contain('audit=updated')
+    })
+
+    editBaseClientDetailsPage.saveButton().click()
+  })
+})
--- integration_tests/pages/editBaseClientDetails.ts
+++ integration_tests/pages/editBaseClientDetails.ts
@@ -1,7 +1,46 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class EditBaseClientDetailsPage extends Page {
   constructor() {
     super('Edit base client details')
   }
+
+  baseClientIdInput = (): PageElement => cy.get('[data-qa="base-client-id-input"]')
+
+  baseClientServiceRadioButton = (): PageElement => cy.get('[data-qa="base-client-service-radio"]')
+
+  baseClientPersonalRadioButton = (): PageElement => cy.get('[data-qa="base-client-personal-radio"]')
+
+  baseClientTypeRadios = (): PageElement => cy.get('[data-qa="base-client-type-radios"]')
+
+  baseClientAccessTokenValidityDropdown = (): PageElement =>
+    cy.get('[data-qa="base-client-access-token-validity-dropdown"]')
+
+  baseClientAccessTokenValidityInput = (): PageElement => cy.get('[data-qa="base-client-access-token-validity-input"]')
+
+  baseClientApprovedScopesInput = (): PageElement => cy.get('[data-qa="base-client-approved-scopes-input"]')
+
+  auditTrailDetailsInput = (): PageElement => cy.get('[data-qa="audit-trail-details-input"]')
+
+  grantTypeInput = (): PageElement => cy.get('[data-qa="grant-type-input"]')
+
+  grantAuthoritiesInput = (): PageElement => cy.get('[data-qa="grant-authorities-input"]')
+
+  grantDatabaseUsernameInput = (): PageElement => cy.get('[data-qa="grant-database-username-input"]')
+
+  grantRedirectUrisInput = (): PageElement => cy.get('[data-qa="grant-redirect-uris-input"]')
+
+  grantJwtFieldsInput = (): PageElement => cy.get('[data-qa="grant-jwt-fields-input"]')
+
+  grantAzureAdLoginFlowCheckboxes = (): PageElement => cy.get('[data-qa="grant-azure-ad-login-flow-checkboxes"]')
+
+  configDoesExpireCheckbox = (): PageElement => cy.get('[data-qa="config-does-expire-checkbox"]')
+
+  configExpiryDaysInput = (): PageElement => cy.get('[data-qa="config-expiry-days-input"]')
+
+  configAllowedIpsInput = (): PageElement => cy.get('[data-qa="config-allowed-ips-input"]')
+
+  saveButton = (): PageElement => cy.get('[data-qa="save-button"]')
+
+  cancelLink = (): PageElement => cy.get('[data-qa="cancel-link"]')
 }
