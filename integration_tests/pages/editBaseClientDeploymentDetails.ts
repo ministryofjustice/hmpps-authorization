b767fdca9e1f375a18479f799b4146ca49cde506
thomasridd <twridd@gmail.com>
2023-11-13 12:14:19 +0000

    integration tests for edit deployment

--- integration_tests/pages/editBaseClientDeploymentDetails.ts
+++ integration_tests/pages/editBaseClientDeploymentDetails.ts
@@ -1,7 +1,33 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class EditBaseClientDeploymentDetailsPage extends Page {
   constructor() {
     super('Edit deployment details')
   }
+
+  baseClientSummaryList = (): PageElement => cy.get('[data-qa="base-client-summary-list"]')
+
+  deploymentTeamInput = (): PageElement => cy.get('[data-qa="deployment-team-input"]')
+
+  deploymentTeamContactInput = (): PageElement => cy.get('[data-qa="deployment-team-contact-input"]')
+
+  deploymentTeamSlackInput = (): PageElement => cy.get('[data-qa="deployment-team-slack-input"]')
+
+  platformHostingRadios = (): PageElement => cy.get('[data-qa="platform-hosting-radios"]')
+
+  platformNamespaceInput = (): PageElement => cy.get('[data-qa="platform-namespace-input"]')
+
+  platformDeploymentInput = (): PageElement => cy.get('[data-qa="platform-deployment-input"]')
+
+  platformSecretNameInput = (): PageElement => cy.get('[data-qa="platform-secret-name-input"]')
+
+  platformSecretKeyInput = (): PageElement => cy.get('[data-qa="platform-secret-key-input"]')
+
+  platformClientIdInput = (): PageElement => cy.get('[data-qa="platform-client-id-input"]')
+
+  platformDeploymentInfoInput = (): PageElement => cy.get('[data-qa="platform-deployment-info-input"]')
+
+  saveButton = (): PageElement => cy.get('[data-qa="save-button"]')
+
+  cancelLink = (): PageElement => cy.get('[data-qa="cancel-link"]')
 }
